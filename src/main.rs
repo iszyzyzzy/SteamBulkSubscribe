3b077ccb3d98b512d455190cb94b325f88a81372
403F <4o3f@proton.me>
2023-08-30 22:28:17 +0800

    feat: Add basic balk subscribe function

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SteamData {
+    web_apikey: String,
+    file_ids: Vec<i64>,
+}
+
+#[tokio::main]
+async fn main() {
+    if !check_data_file_existence() {
+        return;
+    }
+    let data = std::fs::read_to_string("data.json").expect("Reading file error! Aborting");
+    let data: SteamData = serde_json::from_str(data.as_str()).expect("Parsing json error! Aborting");
+    for file_id in data.file_ids {
+        match subscribe_file(file_id, &data.web_apikey).await {
+            Ok(_) => println!("Successfully subscribed file {}. Proceeding", file_id),
+            Err(e) =>
+                panic!("Error subscribing file {}, status code {}", file_id, e.to_string())
+        }
+    }
+    println!("All provided file id subscribed successfully. Finishing")
+}
+
+fn check_data_file_existence() -> bool {
+    return if std::fs::metadata("data.json").is_ok() {
+        println!("data.json file found. Proceeding");
+        true
+    } else {
+        std::fs::write("data.json", r#"{
+  "web_apikey": "PLACE KEY HERE",
+  "file_ids": [
+    2669101904,
+    2341745038
+  ]
+}"#).unwrap();
+        println!(r#"Data input file not found, please place
+1. All the file id you'd like to subscribe
+2. Your Steam Web API key
+in data.json.
+An example data.json file has been generated"#);
+        false
+    };
+}
+
+async fn subscribe_file(file_id: i64, api_key: &String) -> Result<(), Box<dyn std::error::Error>> {
+    let status_code = reqwest::Client::new()
+        .post(format!("https://api.steampowered.com/IPublishedFileService/Subscribe/v1/?key={}&publishedfileid={}&list_type=1", api_key, file_id))
+        .header("content-length", 0)
+        .send()
+        .await.unwrap().status();
+    return if status_code.is_success() {
+        Ok(())
+    } else {
+        println!("Request Steam API failed! Aborting");
+        Err(status_code.as_str().into())
+    };
+}
